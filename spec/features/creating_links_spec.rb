5e68dc9466448c2b9ac12457be9ceb812b31f380
Norm Fasey <norm@fasey.net>
2015-11-03 14:16:27 +0000

    Fixed new link functionality

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,10 +1,9 @@
 feature 'add new links' do
   scenario 'add link' do
     visit '/links/new'
-    fill_in :title, with: 'Google'
-    fill_in :url, with: 'www.google.co.uk'
+    fill_in :title, with: 'Yahoo'
+    fill_in :url, with: 'www.yahoo.co.uk'
     click_button 'Add'
-    save_and_open_page
-    expect(page).to have_content "Google"
+    expect(page).to have_content "Yahoo"
   end
 end
