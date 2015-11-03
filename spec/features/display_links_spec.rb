4b58014dfc6365105af3323b268562f2297dba0e
Norm Fasey <norm@fasey.net>
2015-11-03 11:04:21 +0000

    First feature test

---
+++ spec/features/display_links_spec.rb
@@ -0,0 +1,11 @@
+require 'link'
+
+feature 'Displaying Links' do
+  scenario 'Display all the links on the Links page' do
+    Link.create(title: 'Google', url: 'https://google.co.uk')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    expect(page).to have_content 'Google'
+  end
+end
+
