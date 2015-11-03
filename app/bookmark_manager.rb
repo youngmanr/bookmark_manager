dc27e09c57080133bc6a71d570900e3577bf67ff
Robert Youngman <rob_youngman@yahoo.com>
2015-11-03 15:22:43 +0000

    added RACK_ENV

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,5 +1,9 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require_relative './models/link'
+
+
 
 class BookmarkManager < Sinatra::Base
   get '/links' do
--- app/models/link.rb
+++ app/models/link.rb
@@ -11,6 +11,7 @@ class Link
   property :url, String
 end
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager")
+
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
