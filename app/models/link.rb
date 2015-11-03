eae9446d04460e6c29b9067be807bfee89cb597a
Norm Fasey <norm@fasey.net>
2015-11-03 15:44:22 +0000

    updated link.rb to new database config

--- app/models/link.rb
+++ app/models/link.rb
@@ -12,6 +12,6 @@ class Link
 end
 
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
