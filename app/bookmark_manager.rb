955725d859cb1aaf470ad419994590c33b8d83b0
Robert Youngman <rob_youngman@yahoo.com>
2015-11-04 11:26:27 +0000

    various changes to make rspec pass

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -1,7 +1,8 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-require_relative './models/link'
+require_relative 'data_mapper_setup'
+
 
 class BookmarkManager < Sinatra::Base
 
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,8 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'dm-migrations'
+require_relative './models/link'
+require_relative './models/tag'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
